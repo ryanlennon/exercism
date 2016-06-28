3c1984d5f0d4d92ac054e60e5b562a6047c65171
ryanlennon <ryanlennon07@gmail.com>
2016-06-27 21:41:34 -0500

    solve nucleotide count

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -1,2 +1,39 @@
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
+
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class DNA {
+    final String DNA = "ACGT";
+    final String basePairs;
+
+    public DNA(String basePairs){
+        this.basePairs = basePairs;
+    }
+
+
+    public int count(Character base) {
+        if (DNA.indexOf(base) == -1) {throw new IllegalArgumentException();}
+        String strBase = base.toString();
+        String sequenceWithoutBase = basePairs.replace(strBase, "");
+        return basePairs.length() - sequenceWithoutBase.length();
+    }
+
+    public Map nucleotideCounts() {
+        Map<Character, Integer> nucleotides = new HashMap();
+        nucleotides.put('A', 0);
+        nucleotides.put('C', 0);
+        nucleotides.put('G', 0);
+        nucleotides.put('T', 0);
+       
+        for (char bases : basePairs.toCharArray()){
+            nucleotides.put(bases, nucleotides.get(bases) + 1);
+        }
+        System.out.println(nucleotides);
+        return nucleotides;
+
+    }
 }
